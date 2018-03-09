f48e56acb9b7ccdd22b77d2cd3189b4e800f7890
HyechurnJang <hyjang@cisco.com>
2018-03-09 11:16:11 +0900

    commit

--- agent.py
+++ agent.py
@@ -19,77 +19,83 @@ logstash_sock = None
 refresh = None
 debug = False
 dump = True
+clone = False
 olist = None
-dumpdata = {}
+dumpdata = [{}]
 dd_lock = pygics.Lock()
 
-class Handler(acidipy.Event):
+class ObjectHandler(acidipy.Event):
     
-    def __handle_dump__(self, status, obj):
-        if status == 'created': dumpdata[self.class_name][obj['dn']] = obj
-        elif status == 'deleted': dumpdata[self.class_name].pop(obj['dn'])
+    def __handle_delta__(self, status, obj):
+        if status == 'created': dumpdata[0][self.class_name][obj['dn']] = obj
+        elif status == 'deleted': dumpdata[0][self.class_name].pop(obj['dn'])
         else:
-            for key, val in obj.items(): dumpdata[self.class_name][obj['dn']][key] = val
+            for key, val in obj.items(): dumpdata[0][self.class_name][obj['dn']][key] = val
     
     def __handle_event__(self, status, obj):
         logstash_sock.send(json.dumps(obj) + '\n')
     
     def handle(self, status, obj):
         if self.class_name == 'healthInst': obj['cur'] = int(obj['cur'])
+        if obj['modTs'] == 'never': obj.pop('modTs')
         obj['class_name'] = self.class_name
-        dd_lock.acquire()
-        if dump: self.__handle_dump__(status, obj)
+        dd_lock.on()
+        if dump: self.__handle_delta__(status, obj)
         else: self.__handle_event__(status, obj)
-        dd_lock.release()
+        dd_lock.off()
         if debug: print 'subscribe|%s|%s' % (self.class_name, str(obj))
 
-class Trigger(pygics.Task):
+class Forwarder(pygics.Task):
     
     def __init__(self):
         pygics.Task.__init__(self, tick=refresh)
-        print 'initialize trigger --> ',
-        if dump: self.__init_dump__()
+        if dump: self.start()
         else: self.__init_event__()
     
-    def __init_dump__(self):
+    def __dump__(self):
+        if debug: print 'update dump --> ',
         try:
+            dumpdata[0] = {}
             for class_name in olist:
-                dumpdata[class_name] = {}
+                dumpdata[0][class_name] = {}
                 objs = apic_ctrl.Class(class_name).list(detail=True)
                 for obj in objs:
                     if class_name == 'healthInst': obj['cur'] = int(obj['cur'])
+                    if obj['modTs'] == 'never': obj.pop('modTs')
                     obj['class_name'] = class_name
-                    dumpdata[class_name][obj['dn']] = obj
-                apic_ctrl.Class(class_name).event(Handler())
+                    dumpdata[0][class_name][obj['dn']] = obj
+                if not clone: apic_ctrl.Class(class_name).event(ObjectHandler())
         except Exception as e:
-            print '[ FAILED ]'
-            if debug: print str(e)
+            if debug: 
+                print '[ FAILED ]'
+                print str(e)
             logstash_sock.close()
             apic_ctrl.close()
             exit(1)
-        
-        self.start()
-        print '[ OK ]'
+        if debug: print '[ OK ]'
     
     def __init_event__(self):
+        if debug: print 'subscribe event --> ',
         try:
-            for class_name in olist: apic_ctrl.Class(class_name).event(Handler())
+            for class_name in olist: apic_ctrl.Class(class_name).event(ObjectHandler())
         except Exception as e:
-            print '[ FAILED ]'
-            if debug: print str(e)
+            if debug:
+                print '[ FAILED ]'
+                print str(e)
             logstash_sock.close()
             apic_ctrl.close()
             exit(1)
-        print '[ OK ]'
+        if debug: print '[ OK ]'
         
     def __run__(self):
-        dd_lock.acquire()
-        for cdata in dumpdata.values():
+        if not clone: dd_lock.on()
+        if dumpdata[0] == {}: self.__dump__()
+        else: self.__dump__()
+        for cdata in dumpdata[0].values():
             for data in cdata.values():
                 msg = json.dumps(data) + '\n'
                 logstash_sock.send(msg)
-        dd_lock.release()
-        if debug: print 'sending dump data'
+        if not clone: dd_lock.off()
 
 if __name__ == '__main__':
     
@@ -98,9 +104,11 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='debug mode')
     parser.set_defaults(debug=False)
     parser.add_argument('-l', '--logstash', help='logstash ip address', required=True)
-    parser.add_argument('-e', '--eventmode', dest='dump', action='store_false', help='event trigger mode')
+    parser.add_argument('-c', '--clonemode', dest='clone', action='store_true', help='clone dump mode : always retrieving all object from ACI to send to logstash. ignoring eventmode argument.')
+    parser.set_defaults(clone=False)
+    parser.add_argument('-e', '--eventmode', dest='dump', action='store_false', help='event trigger mode : sending just delta object to logstash. ignoring refresh argument.')
     parser.set_defaults(dump=True)
-    parser.add_argument('-r', '--refresh', default=10, help='refresh seconds of dump mode')
+    parser.add_argument('-r', '--refresh', default=30, help='refresh seconds of dump mode')
     parser.add_argument('-a', '--apic', help='apic ip address', required=True)
     parser.add_argument('-u', '--username', help='apic user', required=True)
     parser.add_argument('-p', '--password', help='apic password', required=True)
@@ -110,47 +118,52 @@ if __name__ == '__main__':
     debug = args.debug
     logstash_ip = args.logstash
     refresh = int(args.refresh)
-    dump = args.dump
+    clone = args.clone
+    dump = True if clone else args.dump
     apic_ip = args.apic
     apic_user = args.username
     apic_pass = args.password
     olist = args.objects
     
-    print 'setting is ...'
-    print 'debug :', debug
-    print 'logstash :', logstash_ip
-    print '  dump :', dump
-    print '  dump-refresh :', refresh
-    print 'apic :', apic_ip
-    print '  username :', apic_user
-    print '  password :', apic_pass
-    print 'objects :', olist
-    print ''
+    if debug: 
+        print 'setting is ...'
+        print 'debug :', debug
+        print 'logstash :', logstash_ip
+        print '  dump :', dump
+        print '  clone :', clone
+        print '  dump-refresh :', refresh
+        print 'apic :', apic_ip
+        print '  username :', apic_user
+        print '  password :', apic_pass
+        print 'objects :', olist
+        print ''
     
-    print 'try to connect APIC --> ',
+    if debug: print 'try to connect APIC --> ',
     try:
         apic_ctrl = acidipy.Controller(apic_ip, apic_user, apic_pass)
     except Exception as e:
-        print '[ FAILED ]'
-        if debug: print str(e)
+        if debug:
+            print '[ FAILED ]'
+            print str(e)
         exit(1)
     else:
-        print '[ OK ]'
+        if debug: print '[ OK ]'
     
-    print 'try to connect Logstash --> ',
+    if debug: print 'try to connect Logstash --> ',
     try:
         logstash_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         logstash_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         logstash_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         logstash_sock.connect((logstash_ip, 8929))
     except Exception as e:
-        print '[ FAILED ]'
-        if debug: print str(e)
+        if debug: 
+            print '[ FAILED ]'
+            print str(e)
         apic_ctrl.close()
         exit(1)
     else:
-        print '[ OK ]'
+        if debug: print '[ OK ]'
 
-    Trigger()
+    Forwarder()
     
     pygics.Task.idle()
